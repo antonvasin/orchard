59d025619efb06116fa9b2365f5a5416d77615ef
Anton Vasin <944260+antonvasin@users.noreply.github.com>
2023-05-21 17:08:22 +0100

    remove redundant condition from camelize

--- string.ts
+++ string.ts
@@ -1,14 +1,12 @@
 /** Converts kebab-, snake-cased or whitespaced string to camelCase */
 export function camelize(str: string) {
   let camelized = "";
-  const delims = ["_", "-", " "];
 
   let toCapital = false;
-  let start = true;
 
   // TODO: bench with for loop
   for (const char of str) {
-    if (delims.includes(char)) {
+    if (char === "_" || char === "-" || char === " ") {
       toCapital = true;
       continue;
     }
@@ -19,16 +17,8 @@ export function camelize(str: string) {
       continue;
     }
 
-    if (start) {
-      camelized += char.toLowerCase();
-      continue;
-    }
-
     camelized += char.toLowerCase();
   }
 
-  if (start) {
-    start = false;
-  }
   return camelized;
 }
