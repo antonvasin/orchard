91e6970643586afbc89f43c422b99dfca765ef3a
Anton Vasin <944260+antonvasin@users.noreply.github.com>
2023-05-21 16:13:58 +0100

    add djb2 hashing

---
+++ hash.ts
@@ -0,0 +1,10 @@
+/** Creates djb2 hash string */
+export function djb2(str: string): string {
+  let hash = 0;
+  for (let i = 0; i < str.length; i++) {
+    const char = str.charCodeAt(i);
+    hash = (hash << 5) - hash + char;
+    hash |= 0; // Convert to 32bit integer
+  }
+  return String(Math.abs(hash));
+}
--- mod.ts
+++ mod.ts
@@ -2,3 +2,4 @@ export * as env from "./env.ts";
 export * as time from "./time.ts";
 export * as obj from "./obj.ts";
 export * as oci from "./oci.ts";
+export * as hash from "./hash.ts";
