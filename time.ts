99f4bbd67672dc1f3faabe6e51a8f0ac335b60de
Anton Vasin <944260+antonvasin@users.noreply.github.com>
2023-05-21 16:09:06 +0100

    getSecondsDiff supports number and Date

--- time.ts
+++ time.ts
@@ -1,6 +1,20 @@
-/** Returns difference in seconds between two ISO strings */
-export function getSecondsDiff(start: string, end: string) {
+/** Returns difference in seconds between two ISO dates */
+export function getSecondsDiff(
+  start: string | number | Date,
+  end: string | number | Date,
+) {
+  const startParsed = typeof start === "string"
+    ? Date.parse(start)
+    : typeof start === "number"
+    ? start
+    : start.getTime();
+  const endParsed = typeof end === "string"
+    ? Date.parse(end)
+    : typeof end === "number"
+    ? end
+    : end.getTime();
+
   return Math.floor(
-    (Date.parse(end) - Date.parse(start)) / 1000,
+    (endParsed - startParsed) / 1000,
   );
 }
