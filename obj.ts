e5f443990b5c1acef7a354957be6614678303fdd
Anton Vasin <944260+antonvasin@users.noreply.github.com>
2023-05-22 01:31:37 +0100

    optimise flatten

--- obj.ts
+++ obj.ts
@@ -1,17 +1,26 @@
 /** Flattens JS object into flat dot-notated one */
-// deno-lint-ignore ban-types
-export function flatten(obj: object, prefix?: string) {
+export function flatten(
+  obj: Record<string, unknown>,
+  prefix?: string,
+  // TODO: investigate passing result as arg, to avoid creating result object every time
+  // TODO: tests
+): Record<string, unknown> {
   const propName = (prefix) ? prefix + "." : "";
-  let ret: Record<string, unknown> = {};
+  const result: Record<string, unknown> = {};
 
   for (const attr in obj) {
-    if (Array.isArray(obj[attr])) {
-      ret[attr] = obj[attr].join(",");
-    } else if (typeof obj[attr] === "object") {
-      ret = { ...ret, ...flatten(obj[attr], propName + attr) };
+    const val = obj[attr];
+    if (Array.isArray(val)) {
+      result[attr] = val.join(",");
+    } else if (typeof val === "object" && val !== null) {
+      Object.assign(
+        result,
+        flatten(val as Record<string, unknown>, propName + attr),
+      );
     } else {
-      ret[propName + attr] = obj[attr];
+      result[propName + attr] = val;
     }
   }
-  return ret;
+
+  return result;
 }
