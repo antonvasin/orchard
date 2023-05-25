a40e356153b52badaa14dbf837d0de7eaf668e6a
Anton Vasin <944260+antonvasin@users.noreply.github.com>
2023-05-25 16:56:32 +0100

    add exports

--- mod.ts
+++ mod.ts
@@ -1,6 +1,7 @@
 export * as env from "./env.ts";
-export * as time from "./time.ts";
+export * as hash from "./hash.ts";
+export * as http from "./http.ts";
 export * as obj from "./obj.ts";
 export * as oci from "./oci.ts";
-export * as hash from "./hash.ts";
 export * as string from "./string.ts";
+export * as time from "./time.ts";
