190a967c53ac5a378892ed2e00940a11481851fc
Anton Vasin <944260+antonvasin@users.noreply.github.com>
2023-06-13 21:18:16 +0100

    bin and range functions

--- number.test.ts
+++ number.test.ts
@@ -1,6 +1,7 @@
 import { assert, assertEquals } from "./test_deps.ts";
 
 import * as num from "./number.ts";
+import { bin, range } from "./number.ts";
 
 Deno.test("collideRect", () => {
   assert(num.collideRect(100, 100, 100, 100, 50, 50, 200, 200));
@@ -24,4 +25,27 @@ Deno.test("scale", () => {
   assertEquals(doubleLinear(31), 240);
   assertEquals(doubleLinear(40), 300);
   assertEquals(doubleLinear(60), 300);
+});
+
+Deno.test("bin", () => {
+  const bins = ["owl", "cat", "otter", "dog"];
+  const range = [0, 100];
+  assertEquals(bin(10, range[0], range[1], bins), bins[0]);
+  assertEquals(bin(70, range[0], range[1], bins), bins[2]);
+});
+
+Deno.test("range", () => {
+  let i = 0;
+  // for (const r of range(0, 3)) {
+  //   i = r;
+  // }
+  // assertEquals(i, 2);
+
+  i = 10;
+  for (const r of range(3, 0)) {
+    console.log(r);
+
+    i = r;
+  }
+  assertEquals(i, 0);
 });
--- number.ts
+++ number.ts
@@ -1,3 +1,4 @@
+/** Return random number with adjustable bias. Increasing bias increases resulting number */
 export function random(min = 0, max = 1, b = 0): number {
   const bias = clamp(0, 1, b);
 
@@ -6,18 +7,22 @@ export function random(min = 0, max = 1, b = 0): number {
   return max > 1 ? Math.floor(value) : value;
 }
 
+/** Clamps value to be in the range min..max */
 export function clamp(num: number, min = 0, max = 1) {
   return Math.min(max, Math.max(min, num));
 }
 
+/** Interpolate value linearly */
 export function lerp(a: number, b: number, n: number) {
   return a * (1 - n) + b * n;
 }
 
+/** Interpolate value in reverse */
 export function invlerp(a: number, b: number, n: number) {
   return clamp((n - a) / (b - a));
 }
 
+/** Scales value in the range a1..a2 to corresponding value in the range a2..b2 */
 export function scale(
   a1: number,
   b1: number,
@@ -33,10 +38,12 @@ export function distance(x0: number, y0: number, x1: number, y1: number) {
   return Math.hypot(x1 - x0, y1 - y0);
 }
 
+/** Check if number is between two other numbers */
 export function isBetween(n: number, a: number, b: number) {
   return (n > a && n < b) || (n > b && n < a);
 }
 
+/** Detects if two rectangles are overlapping/colliding */
 export function collideRect(
   x1: number,
   y1: number,
@@ -54,10 +61,40 @@ export function collideRect(
   return intersectX && intersectY;
 }
 
+/** Convert degrees to radians */
 export function degToRad(deg: number) {
   return deg * (Math.PI / 180);
 }
 
+/** Convert radians to degress */
 export function radToDeg(rad: number) {
   return rad * (180 / Math.PI);
+}
+
+/** Return value in the range 0..1 */
+export function normalise(min: number, max: number, n: number) {
+  return (n - min) / (max - min);
+}
+
+/**
+ * For given number in the range min..max and array of values (bins) returns
+ * corresponding bin.
+ */
+export function bin<T>(
+  value: number,
+  min: number,
+  max: number,
+  bins: T[],
+): T {
+  const index = Math.floor(normalise(min, max, value) * bins.length);
+  return bins[clamp(0, bins.length - 1, index)];
+}
+
+export function* range(start: number, end: number) {
+  const iterDir = start <= end ? 1 : -1;
+  const length = iterDir > 0 ? end - start : start - end;
+
+  for (let i = start; i < length; i += iterDir) {
+    yield i;
+  }
 }
