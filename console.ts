c03a4432a4da4f8ecc1bb656f023eedf9de97d8d
Anton Vasin <944260+antonvasin@users.noreply.github.com>
2023-06-22 15:38:56 +0100

    simple console css functions

--- console.ts
+++ console.ts
@@ -1,31 +1,7 @@
-type CssConsoleString = [string, string];
-/**
- * @example
- * ```ts
- * console.log(...bold('Request: '), req.path)
- * ```
- */
-export const bold = (str: CssConsoleString) =>
-  wrapOrPush(str, "font-weight: bold");
-export const italic = (str: CssConsoleString) =>
-  wrapOrPush(str, "font-style: italic");
-export const red = (str: CssConsoleString) => wrapOrPush(str, "color: red");
-export const blue = (str: CssConsoleString) => wrapOrPush(str, "color: blue");
-
-function isCssAry(ary: CssConsoleString): ary is [string, string] {
-  return Array.isArray(ary) && ary.length === 2 &&
-    !!ary[ary.length - 1].match(/^([0-9A-Za-z-]+):/);
-}
-
-function wrapOrPush(aryOrStr: CssConsoleString, css: string) {
-  if (isCssAry(aryOrStr)) {
-    const [msg, cssStr] = aryOrStr;
-    return [msg, cssStr + `; ${css}`];
-  } else {
-    return wrap(aryOrStr, css);
-  }
-}
-
-function wrap(msg: string, css: string) {
-  return [`%c${msg}`, css];
-}
+export const bold = "font-weight: bold";
+export const italic = "font-style: italic";
+export const red = "color: red";
+export const blue = "color: blue";
+export const green = "color: green";
+export const reset = "font-weight: normal; color: initial; font-style: normal;";
+export const combineStyle = (...args: string[]) => args.join(";");
