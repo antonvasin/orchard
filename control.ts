ae5a58ebbbe0028b9d94c449eeaf26f2fc5bd8d1
Anton Vasin <944260+antonvasin@users.noreply.github.com>
2025-07-27 04:34:25 +0100

    Add custom errors

--- control.ts
+++ control.ts
@@ -1,9 +1,21 @@
-export function assert(cond: unknown, msg = "Assertion error"): asserts cond {
-  if (!cond) {
-    throw new Error(msg);
-  }
+export function error(name: string) {
+  return class extends Error {
+    constructor(message: string) {
+      super(message);
+      this.name = name;
+    }
+  };
 }
 
+export const AssertError = error("AssertError");
+export function assert<T>(
+  val: T | null | undefined,
+  msg = `Expected ${val} to be truthy`,
+): asserts val is T {
+  if (!val) throw new AssertError(msg);
+}
+
+export const UnreachableError = error("Unreachable");
 export function unreachable() {
-  throw new Error("unreachable");
+  throw new UnreachableError("Unreachable code reached");
 }
